85009c6318cfc15fe931d85f2e376da59a735674
LiChenran <cl4062@nyu.edu>
2024-03-20 08:40:31 -0400

    implement c++ version

--- glb_parser.py
+++ glb_parser.py
@@ -4,16 +4,17 @@ import os
 from pygltflib import GLTF2
 
 
-def glb_plain_parser(input_dir, organ: str, output_dir):
+def glb_plain_parser(input_glb, output_dir):
 
     # file = "C:/Users/catherine/Desktop/Research/ccf-releases/v1.1/models/" + organ + '.glb'
-    file = os.path.join(input_dir, organ + '.glb')
+    #file = os.path.join(input_dir, organ + '.glb')
+    #file = os.path.join(input_glb, organ + '.glb')
     data_type_dict = {5121: 'uint8', 5123: 'uint16', 5125: 'uint32', 5126: 'float32'}
     number_of_components = {'SCALAR': 1, 'VEC2': 2, 'VEC3': 3, 'VEC4': 4, 'MAT2': 4, 'MAT3': 9, 'MAT4': 16}
 
-    glb = GLTF2.load(file)
+    glb = GLTF2.load(input_glb)
     binary_blob = glb.binary_blob()
-    output_organ_dir = os.path.join(output_dir, organ)
+    #output_organ_dir = os.path.join(output_dir, organ)
 
     for mesh in glb.meshes:
 
@@ -41,7 +42,7 @@ def glb_plain_parser(input_dir, organ: str, output_dir):
             count=points_accessor.count * 3,
         ).reshape((-1, 3))
 
-        save_single_mesh(points, triangles, mesh_name, output_organ_dir)
+        save_single_mesh(points, triangles, mesh_name, output_dir)#output_organ_dir)
 
 
 def save_single_mesh(points, triangles, mesh_name, output_dir):
@@ -61,44 +62,44 @@ def save_single_mesh(points, triangles, mesh_name, output_dir):
         for triangle in triangles:
             f.write("3 {} {} {}\n".format(triangle[0], triangle[1], triangle[2]))
 
-        print("   {} has {} points, {} triangle faces\n".format(mesh_name, len(points), len(triangles)))
+        print("  {} has {} points, {} triangle faces".format(mesh_name, len(points), len(triangles)))
 
 
 if __name__ == "__main__":
     # input_dir = "../../ccf-releases/v1.3/models/"
     # output_dir = "../../models/plain"
-    folder_name = "temp-model-off"
+    # folder_name = "temp-model-off"
 
-    # Check if the folder already exists, and if not, create it
-    if not os.path.exists(folder_name):
-        os.mkdir(folder_name)
-        print(f"Temporal folder '{folder_name}' created successfully.\n")
-    else:
-        print(f"Temporal folder '{folder_name}' already exists.\n")  
+    # # Check if the folder already exists, and if not, create it
+    # if not os.path.exists(folder_name):
+    #     os.mkdir(folder_name)
+    #     print(f"Temporal folder '{folder_name}' created successfully.\n")
+    # else:
+    #     print(f"Temporal folder '{folder_name}' already exists.\n")  
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_dir', type=str, help="input directory of glb files", default="./model-glb")   #../../ccf-releases/v1.3/models/")
-    parser.add_argument('--output_dir', type=str, help="output directory of off files", default="./" + folder_name)
+    parser.add_argument('--input_glb', type=str, help="input file path of the glb file", default="./model-glb")   #../../ccf-releases/v1.3/models/")
+    #parser.add_argument('--output_dir', type=str, help="output directory of off files", default="./temp-model-off")
     args = parser.parse_args()
 
-    print(f'{folder_name}'"\n")
+    #print(f'{folder_name}'"\n")
 
-    input_dir = args.input_dir
-    output_dir = args.output_dir
+    input_glb = args.input_glb
+    #output_dir = args.output_dir
+    output_dir = "./temp-model-off"
+    #files = os.listdir(input_glb)
 
-    files = os.listdir(input_dir)
+    #for f in files:
+        #if f.endswith('.glb'):
+            #print(f)
 
-    for f in files:
-        if f.endswith('.glb'):
-            print(f)
+            #organ = f[:-4]
+            #print(organ)
+            #print("start parsing {}\n".format(organ))
 
-            organ = f[:-4]
-            print(organ)
-            print("start parsing {}\n".format(organ))
+    glb_plain_parser(input_glb, output_dir)
 
-            glb_plain_parser(input_dir, organ, output_dir)
-
-            print("end parsing {}\n".format(organ))
+            #print("end parsing {}\n".format(organ))
 
     # if os.path.exists(folder_name):
     #     os.rmdir(folder_name)
---
+++ main.cpp
@@ -0,0 +1,139 @@
+#include "mymesh.h"
+
+#include <time.h>
+#include <iostream>
+//#include <iomanip>
+#include <sys/time.h>
+
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <filesystem>
+
+
+
+
+// Function to compute minimum distance between vertex-pairs of two Surface_mesh objects
+double computeMinimumDistance(const Surface_mesh& mesh1, const Surface_mesh& mesh2) {
+    double minDistance = std::numeric_limits<double>::max();
+
+    // Iterate over all vertices of the first mesh
+    for (auto v1 : mesh1.vertices()) {
+        Point p1 = mesh1.point(v1);
+
+        // Iterate over all vertices of the second mesh
+        for (auto v2 : mesh2.vertices()) {
+            Point p2 = mesh2.point(v2);
+
+            // Compute the distance between the current pair of vertices
+            double dist = std::sqrt(CGAL::squared_distance(p1, p2));
+
+            // Update the minimum distance if necessary
+            if (dist < minDistance)
+                minDistance = dist;
+        }
+    }
+
+    return minDistance;
+}
+
+
+int main(int argc, char **argv)
+{
+    srand( (unsigned)time( NULL ) );
+
+
+    struct timeval start, end;
+    long long microseconds;
+    gettimeofday(&start, nullptr);
+
+    
+
+    // Open the CSV file for writing
+    std::ofstream outputFile("output.csv");
+    if (!outputFile.is_open()) {
+        std::cerr << "Error: Unable to open output.csv for writing." << std::endl;
+        return 1;
+    }
+
+    // Write header to CSV file
+    outputFile << "Mesh A ID,Mesh B ID,Minimum Distance" << std::endl;
+  
+
+    std::vector<Mymesh> meshes;
+    std::vector<std::string> meshIDs;
+
+    std::string directoryPath = "./temp-model-off";
+    std::cout << "\n------------------------------------------------\n" << std::endl;
+
+    std::cout << "Loading mesh: " << std::endl;
+
+    // Iterate over all files in the specified directory
+    for (const auto& entry : std::filesystem::directory_iterator(directoryPath)) {
+        std::string filename = entry.path().filename().string();
+        // Check if the file has a .off extension
+        if (filename.size() > 4 && filename.substr(filename.size() - 4) == ".off") {
+            // Store the file path
+            std::string filePath = entry.path().string();
+
+            // Process the .off file
+            //processOffFile(filePath);
+            meshes.push_back(Mymesh(filePath));
+            meshIDs.push_back(filename);
+            std::cout << "  " << filePath.substr(0, filePath.size() - 4)<< std::endl;
+        }
+    }
+
+    std::cout << "mesh number: " << meshes.size() << std::endl;
+    for (Mymesh &mesh: meshes) mesh.create_aabb_tree();
+
+    // Compare each pair of meshes
+    for (size_t i = 0; i < meshes.size(); ++i) {
+        for (size_t j = i + 1; j < meshes.size(); ++j) {
+            //const auto& meshA = meshes[i];
+            //const auto& meshB = meshes[j];
+
+            const auto aabbtreeA = meshes[i].get_aabb_tree();
+            const auto aabbtreeB = meshes[j].get_aabb_tree();
+
+            // Check if meshes intersect
+            if (aabbtreeA->do_intersect(*aabbtreeB)) {
+                // If meshes intersect, set minimum distance to -1
+                outputFile << meshIDs[i] << "," << meshIDs[j] << ",-1" << std::endl;
+            } else {
+                // If meshes do not intersect, compute minimum distance between vertex-pairs
+                double minDistance = computeMinimumDistance(meshes[i].get_raw_mesh(), meshes[j].get_raw_mesh());
+
+                outputFile << meshIDs[i] << "," << meshIDs[j] << "," << std::setprecision(12) << minDistance << std::endl;
+            }
+        }
+    }
+
+    // Close the CSV file
+    outputFile.close();
+
+    // Construct the command to delete the folder and its contents
+    std::string command = "rm -r ./temp-model-off";
+
+    // Execute the command using system()
+    int result = std::system(command.c_str());
+
+    if (result == 0) {
+        std::cout << "\ntemp-model-off folder deleted successfully." << std::endl;
+    } else {
+        std::cerr << "Error deleting temp-model-off folder." << std::endl;
+    }
+
+    std::cout << "CSV file created successfully." << std::endl;
+
+
+    //
+    gettimeofday(&end, nullptr);
+    microseconds = (end.tv_sec - start.tv_sec) * 1000000LL + (end.tv_usec - start.tv_usec);
+    // Print the elapsed time
+    std::cout << "---------------main.cpp---------------Time elapsed: " << microseconds << " microseconds" << std::endl << std::endl << std::endl;
+
+
+}
+
+
